bd81ee73b2007da70939b6c193eb73ebb1f00975
Bhavana <bhavanabasiya@gmail.com>
2019-05-29 22:57:12 +0530

    committed1

--- app2/server.js
+++ app2/server.js
@@ -2,10 +2,10 @@ const express=require('express');
 
 const app=express();
 
+console.log('HELLO');
 app.listen(3000,'0.0.0.0',()=>{
     console.log('server started on 3000');
 
-
-console.log('HELLO');
+ 
 
 });
